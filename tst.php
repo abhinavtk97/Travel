52b54976d4c846c5f621fb3e33d5851b31cfdc81
abhinavtk97 <abhinavtk97@gmail.com>
2017-08-28 20:34:27 +0530

    fixed root

--- tst.php
+++
@@ -1,7 +0,0 @@
-<html>
-<?php
-ini_set("allow_url_fopen",1);
-$json=file_get_contents('https://location-leaderboard.services.mozilla.com/api/v1/leaders/country/IN/');
-$obj = json_decode($json);
-echo $obj->results[2]->name;
-?>
